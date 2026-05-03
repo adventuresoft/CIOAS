b3cdebf3d3cea6de09d3d0906d1f02143bf4f80e
TareqJamilSarkar <tareqjamilsarkar@gmail.com>
2026-05-03 10:54:04 +0600

    up

--- resources/views/backend/pages/vehicle/create.blade.php
+++ resources/views/backend/pages/vehicle/create.blade.php
@@ -57,7 +57,7 @@
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="{{ route('vehicle.index') }}">Vehicle</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('vehicle.index') }}">Vehicle</a></li> 
                     <li class="breadcrumb-item active">Create</li>
                 </ol>
             </div>
